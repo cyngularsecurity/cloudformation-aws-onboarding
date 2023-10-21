5dfd33ce75aebccbae986546b1c23e9807bb61c7
dvir gross <dvirgross2000@gmail.com>
2023-10-21 18:00:30 +0300

    sync script configured for prod bucket too

--- Scripts/SyncWithS3.sh
+++ Scripts/SyncWithS3.sh
@@ -6,8 +6,12 @@ BLUE="\033[34m"
 RESET="\033[0m"
 
 BUCKET_NAME="onboarding-stacks"
+PROD_BUCKET_NAME="cyngular-onboarding-templates"
 LOCAL_FILE_PATH="../Stacks"
 
 echo -e "${BLUE}Uploading stacks to S3...${RESET}"
 aws s3 sync "$LOCAL_FILE_PATH" "s3://$BUCKET_NAME/"
 echo -e "${GREEN}successfully synced with s3://$BUCKET_NAME/${RESET}"
+
+aws s3 sync "$LOCAL_FILE_PATH" "s3://$PROD_BUCKET_NAME/"
+echo -e "${GREEN}successfully synced with s3://$PROD_BUCKET_NAME/${RESET}"
