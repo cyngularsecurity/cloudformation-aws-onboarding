1c71e6aa8e22a2f0c6e4dfd4b31b2c800ae69908
dvir gross <dvirg@cyngularsecurity.com>
2024-10-01 19:50:56 +0300

    latest

--- Scripts/SyncWithS3.sh
+++ Scripts/SyncWithS3.sh
@@ -10,6 +10,6 @@ BRANCH=$(git branch --show-current)
 BUCKET_NAME="cyngular-onboarding-templates"
 LOCAL_FILE_PATH="CFN"
 
-
+aws s3 sync "$LOCAL_FILE_PATH" "s3://$BUCKET_NAME/stacks" --profile prod
 aws s3 sync "$LOCAL_FILE_PATH" "s3://$BUCKET_NAME/$BRANCH/stacks" --profile prod
 echo -e "${GREEN}synced new On Boarding files with  ----> s3://${BLUE}$BUCKET_NAME/${RED}$BRANCH/${RESET}stacks"
