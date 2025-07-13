373158ee5cbf8e4e7701e32ea8d92c32c91f222e
Dvir Gross <dvirg@cyngularsecurity.com>
2025-07-13 14:12:53 +0300

    DEVOPS-885 - latest

--- Scripts/Gen3/Layer.sh
+++ Scripts/Gen3/Layer.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -eu
 
+LAYER_NAME="aioboto3"
+
 # Create necessary directories
 mkdir -p layer/python zips
 
@@ -9,6 +11,12 @@ source .venv/bin/activate
 
 # Install packages from requirements.txt
 pip install -r requirements.txt -t layer/python
+# pip install --platform manylinux2014_x86_64 \
+#   -r requirements.txt \
+#   --target=layer/python \
+#   --implementation cp --python-version 3.11 \
+#   --only-binary=:all: --upgrade aioboto3
+
 cd layer
 
 zip -r ../zips/aioboto3.zip .
@@ -17,9 +25,17 @@ cd ..
 deactivate
 
 aws lambda publish-layer-version \
-    --layer-name aioboto3 \
+    --layer-name ${LAYER_NAME} \
     --description "A layer including the aioboto3 module" \
     --zip-file fileb://zips/aioboto3.zip \
-    --compatible-runtimes python3.9 python3.10 python3.11 python3.12 python3.13
+    --compatible-runtimes python3.9 python3.10 python3.11 python3.12 python3.13 \
+    --license-info "MIT"
 
 rm -rf .venv layer zips
+
+aws lambda add-layer-version-permission \
+  --layer-name ${LAYER_NAME} \
+  --version-number 1 \
+  --statement-id public-access \
+  --action lambda:GetLayerVersion \
+  --principal "*"
